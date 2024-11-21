202382abf6b3bcfa46e44e696201422d85454391
shashank <reddysheshank0@gmail.com>
2024-11-21 22:38:02 +0530

    innn

---
+++ Prime.java
@@ -0,0 +1,20 @@
+public class Prime{
+    public static void main(String args[]){
+        int n = 7;
+       int count=0;
+        for(int i=2;i<=n;i++){
+           if(i%n==0){
+             count++;
+           } 
+           }
+         if(count==1){
+            System.out.println("prime");
+            
+        }
+        else{
+             System.out.println(" not prime");
+            
+        }
+
+}
+}
